83c3a28990b76bb62084ed20886662d99cb0ad00
Jay <isaacsdaddy42293@gmail.com>
2017-07-13 13:05:07 -0400

    first code

---
+++ inheritancePolymorphism/Boat.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inheritancePolymorphism
+{
+    class Boat : Vehicle
+    {
+        private double waterDrag;
+
+        public Boat(int seats, int carryCapacity, string color, double movementSpeed, double waterDrag)
+        {
+            this.seats = seats;
+            this.carryCapacity = carryCapacity;
+            this.color = color;
+            this.movementSpeed = movementSpeed;
+            this.waterDrag = waterDrag;
+        }
+
+
+        public override void Move()
+        {
+            distanceTraveled += movementSpeed * waterDrag;
+        }
+    }
+}
